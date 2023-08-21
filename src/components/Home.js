308a48c8aab4fa3c2ba639d3fc17fd2193b697cc
rthota7 <rthota7@dxc.com>
2023-08-22 00:05:31 +0530

    made same background

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Banner from './banner/Banner';
 import SignIn from './signin/SignIn';
 
-const Home = ({ setUserSignedIn, useMicroserviceA }) => {
+const Home = () => {
   return (
     <div className="flex bg-gray-100 min-h-screen">
       {/* Left Section - Banner */}
-      <div className="w-2/3 bg-white p-8">
+      <div className="w-2/3 bg-gray-100 p-8"> {/* Apply the background color class here */}
         <Banner />
       </div>
 
       {/* Right Section - Sign In */}
       <div className="w-1/3 p-8">
         <div className="flex justify-center items-center h-full">
-          <SignIn setUserSignedIn={setUserSignedIn} useMicroserviceA={useMicroserviceA} />
+          <SignIn />
         </div>
       </div>
     </div>
